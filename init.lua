f45fecd1cc92b50416aa45fc59cabbcaab2ada5c
redmaw <42783523+redmawzx@users.noreply.github.com>
2024-12-09 01:54:51 +0100

    treesitter

--- init.lua
+++ init.lua
@@ -1,12 +1,18 @@
 require("config.lazy")
 
+-- Configure options
+vim.opt.shiftwidth = 2
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
+
 -- Highlight yanking text
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+  vim.highlight.on_yank()
+end,
 })
 
 -- Keymaps
@@ -16,3 +22,4 @@ local opts = {noremap = true, silent = true}
 vim.keymap.set("n", "<space>x", ":.lua<CR>") -- source a single line
 vim.keymap.set("v", "<space>x", ":lua<CR>") -- source selection
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") -- source entire file
+
---
+++ lua/config/plugins/treesitter.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      require'nvim-treesitter.configs'.setup {
+	ensure_installed = { "bash", "c", "css", "dockerfile", "gitignore", "graphql", "html", "htmldjango", "http", "java", "javascript", "json", "lua", "markdown", "markdown_inline", "php", "prisma", "python", "scss", "sql", "svelte", "tmux", "tsx", "typescript", "vim", "vimdoc", "query", "yaml" },
+	auto_install = false,
+	highlight = {
+	  enable = true,
+	  disable = function(lang, buf)
+	    local max_filesize = 100 * 1024 -- 100 KB
+	    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	    if ok and stats and stats.size > max_filesize then
+	      return true
+	    end
+	  end,
+
+	  additional_vim_regex_highlighting = false,
+	},
+      }
+    end,
+  }
+}

ab541fe67ba6faa6e4f87f660c28712d3db7a94f
redmaw <42783523+redmawzx@users.noreply.github.com>
2024-12-10 21:46:38 +0100

    vim options

--- init.lua
+++ init.lua
@@ -1,19 +1,26 @@
 require("config.lazy")
 
 -- Configure options
+vim.opt.smartindent = true
 vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.clipboard = "unnamedplus"
 vim.opt.termguicolors = true
 vim.opt.expandtab = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.ignorecase = true -- remove case sensitive
+vim.opt.smartcase = true -- case sensitive, if specific
+vim.opt.wrap = false
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+vim.opt.signcolumn = "yes"
+vim.opt.scrolloff = 8
 
 -- Keymaps
 vim.g.mapleader = " "
 
--- Keymaps: Sourcing
 vim.keymap.set("n", "<leader>x", ":.lua<CR>") -- source a single line
 vim.keymap.set("v", "<leader>x", ":lua<CR>") -- source selection
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>") -- source entire file

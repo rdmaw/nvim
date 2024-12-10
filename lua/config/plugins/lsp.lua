50bb96b1c5e18e60c92198430e3588e17dd5066d
redmaw <42783523+redmawzx@users.noreply.github.com>
2024-12-11 00:38:09 +0100

    prettierd

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -1,59 +1,80 @@
 return {
 
-  -- mason
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
+	-- mason
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
 
-  -- mason lspconfig
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "cssls", "css_variables", "tailwindcss", "emmet_language_server", "html", "lua_ls", "pyright", "ts_ls"
-        },
-      })
-    end,
-  },
+	-- mason lspconfig
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"cssls",
+					"css_variables",
+					"tailwindcss",
+					"emmet_language_server",
+					"html",
+					"lua_ls",
+					"pyright",
+					"ts_ls",
+				},
+			})
+		end,
+	},
 
-  -- lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
-        opts = {
-          library = {
-            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-          },
-        },
-      },
-    },
-    config = function()
-      local lspconfig = require("lspconfig")
+	-- lspconfig
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.cssls.setup({})
-      lspconfig.css_variables.setup({})
-      lspconfig.tailwindcss.setup({})
-      lspconfig.lua_ls.setup({})
-      lspconfig.emmet_language_server.setup({
-        filetypes = { "css", "eruby", "html", "htmldjango", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "pug", "typescript", "typescriptreact" },
-      })
-      lspconfig.html.setup({})
-      lspconfig.pyright.setup({})
-      lspconfig.ts_ls.setup({})
+			lspconfig.cssls.setup({})
+			lspconfig.css_variables.setup({})
+			lspconfig.tailwindcss.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.emmet_language_server.setup({
+				filetypes = {
+					"css",
+					"eruby",
+					"html",
+					"htmldjango",
+					"javascript",
+					"javascriptreact",
+					"less",
+					"sass",
+					"scss",
+					"svelte",
+					"pug",
+					"typescript",
+					"typescriptreact",
+				},
+			})
+			lspconfig.html.setup({})
+			lspconfig.pyright.setup({})
+			lspconfig.ts_ls.setup({})
 
-      -- code action
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      -- experimental, might delete; gotodefinition, gotoreferences, rename
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
-    end,
-  },
+			-- code action
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+			-- experimental, might delete; gotodefinition, gotoreferences, rename
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+		end,
+	},
 }
--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -1,48 +1,45 @@
 return {
 
-  -- telescope
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = { "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
-  },
-  config = function()
-    require("telescope").setup {
-      pickers = {
-        find_files = {
-          theme = "ivy"
-        }
-      },
-      extensions = {
-        fzf = {}
-      }
-    }
+	-- telescope
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.8",
+		dependencies = { "nvim-lua/plenary.nvim", { "nvim-telescope/telescope-fzf-native.nvim", build = "make" } },
+		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						theme = "ivy",
+					},
+				},
+				extensions = {
+					fzf = {},
+				},
+			})
 
-    -- Open telescope help tags (fh:find help)
-    vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
-    -- Find inside current directory (fd:find dir)
-    vim.keymap.set("n", "<leader>fd", require("telescope.builtin").find_files)
-    -- Find config directory from anywhere (en:edit neovim)
-    vim.keymap.set("n", "<leader>en", function()
-      require("telescope.builtin").find_files {
-        cwd = vim.fn.stdpath("config")
-      }
-    end)
-  end
-},
+			-- Open telescope help tags (fh:find help)
+			vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
+			-- Find inside current directory (fd:find dir)
+			vim.keymap.set("n", "<leader>fd", require("telescope.builtin").find_files)
+			-- Find config directory from anywhere (en:edit neovim)
+			vim.keymap.set("n", "<leader>en", function()
+				require("telescope.builtin").find_files({
+					cwd = vim.fn.stdpath("config"),
+				})
+			end)
+		end,
+	},
 
--- telescope-ui
-{
-  "nvim-telescope/telescope-ui-select.nvim",
-  config = function()
-    require("telescope").setup({
-      extensions = {
-        require("telescope.themes").get_dropdown {
-        }
-      }
-    })
-    require("telescope").load_extension("ui-select")
-  end,
-},
+	-- telescope-ui
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					require("telescope.themes").get_dropdown({}),
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }

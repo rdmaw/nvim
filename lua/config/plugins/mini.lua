eac2fa0baa816f1a6522da746ba3bc93fda25e08
redmaw <42783523+redmawzx@users.noreply.github.com>
2024-12-12 21:48:12 +0100

    mini.hipatterns

--- lua/config/plugins/mini.lua
+++ lua/config/plugins/mini.lua
@@ -2,8 +2,16 @@ return {
 	{
 		"echasnovski/mini.nvim",
 		config = function()
-			-- local statusline = require "mini.statusline"
-			-- statusline.setup { use_icons = true }
+			local hipatterns = require("mini.hipatterns")
+			hipatterns.setup({
+				highlighters = {
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+					hex_color = hipatterns.gen_highlighter.hex_color(),
+				},
+			})
 		end,
 	},
 }

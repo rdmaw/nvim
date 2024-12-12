857efa798b524fdb440f20aeab05c55c8ff82b1a
redmaw <42783523+redmawzx@users.noreply.github.com>
2024-12-12 21:11:58 +0100

    statuslines modes and add silent/noremap

--- lua/config/plugins/lualine.lua
+++ lua/config/plugins/lualine.lua
@@ -5,6 +5,10 @@ return {
 		config = function()
 			local auto_theme_custom = require("lualine.themes.auto")
 			auto_theme_custom.normal.c.bg = "none"
+			auto_theme_custom.insert.c.bg = "none"
+			auto_theme_custom.visual.c.bg = "none"
+			auto_theme_custom.command.c.bg = "none"
+			auto_theme_custom.replace.c.bg = "none"
 
 			require("lualine").setup({
 				options = {

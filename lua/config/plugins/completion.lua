00c9ab855092f2b71fc3dadccb006a374cfeb16f
rdmaw <42783523+rdmaw@users.noreply.github.com>
2025-03-06 22:19:49 +0100

    disable bugged plugins

--- lua/config/plugins/completion.lua
+++ lua/config/plugins/completion.lua
@@ -1,33 +1,33 @@
-return {
-  {
-    "saghen/blink.cmp",
-    dependencies = "rafamadriz/friendly-snippets",
-
-    version = "v0.*",
-
-    opts = {
-
-      completion = {
-        menu = {
-          auto_show = function(ctx)
-            return ctx.mode ~= "cmdline"
-          end,
-        },
-      },
-
-      keymap = { preset = "default" },
-
-      appearance = {
-        use_nvim_cmp_as_default = true,
-        nerd_font_variant = "normal",
-      },
-
-      sources = {
-        default = { "lsp", "path", "snippets", "buffer" },
-      },
-
-      signature = { enabled = true },
-    },
-    opts_extend = { "sources.default" },
-  },
-}
+-- return {
+--   {
+--     "saghen/blink.cmp",
+--     dependencies = "rafamadriz/friendly-snippets",
+--
+--     version = "v0.*",
+--
+--     opts = {
+--
+--       completion = {
+--         menu = {
+--           auto_show = function(ctx)
+--             return ctx.mode ~= "cmdline"
+--           end,
+--         },
+--       },
+--
+--       keymap = { preset = "default" },
+--
+--       appearance = {
+--         use_nvim_cmp_as_default = true,
+--         nerd_font_variant = "normal",
+--       },
+--
+--       sources = {
+--         default = { "lsp", "path", "snippets", "buffer" },
+--       },
+--
+--       signature = { enabled = true },
+--     },
+--     opts_extend = { "sources.default" },
+--   },
+-- }
